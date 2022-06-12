ca30b309147bcd33eaceb75a981d744c588c56ca
Da Kim <daeunkim127@gmail.com>
2022-06-12 19:05:14 -0400

    review page change to handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -141,6 +141,35 @@ router.get('/category/:id', async (req, res) => {
   }
 });
 
+//review page
+router.get('/item/:id/review', async (req, res) => {
+  try {
+    const itemData = await Item.findByPk(req.params.id, {
+      include: [
+        {
+          model: Category
+        },
+        {
+          model: User,
+          attributes: { exclude: ['password'] },
+        },
+        {
+          model: Review
+        }
+      ]
+    });
+    
+    const item = itemData.get({ plain: true });
+
+    res.render('review', {
+      item,
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // Use withAuth middleware to prevent access to route
 router.get('/profile', withAuth, async (req, res) => {
   try {
